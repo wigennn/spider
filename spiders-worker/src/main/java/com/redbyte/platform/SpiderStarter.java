a31d701c82f9e30ef4f984bdfcfe978f2ed626d7
wangwq <wangweiqing@yunzhangfang.com>
2020-10-16 10:29:58 +0800

    调整

---
+++ spiders-worker/src/main/java/com/redbyte/platform/SpiderStarter.java
@@ -0,0 +1,19 @@
+package com.redbyte.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author wangwq
+ */
+@SpringBootApplication
+public class SpiderStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpiderStarter.class, args);
+    }
+}
